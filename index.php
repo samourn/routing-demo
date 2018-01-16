2cd08d3521cf4acf6b30d21921f58007ea66e712
Sam Ourn <sourn@mail.greenriver.edu>
2018-01-16 13:25:50 -0500

    Created a new Template

--- index.php
+++ index.php
@@ -6,13 +6,34 @@ require_once ('vendor/autoload.php');
 //Create an instance of the Base class
 $f3 = Base::instance();
 
+//Set debug level
+$f3->set('DEBUG', 3); //0(off), 3(max)
+
 //Define a default route
 $f3->route('GET /', function() {
-    {
         echo'<h1>Routing Demo</h1>';
-        //$view = new View;
+        //$view = new View();
         //echo $view->render ('views/home.html');
-    }
+}
+);
+
+//Define a page1 route
+$f3->route('GET /page1', function() {
+    echo'<h1>This is page 1</h1>';
+}
+);
+
+//Define a page1 route
+$f3->route('GET /page1/subpage-a', function() {
+    echo'<h1>This is page 1, subpage a</h1>';
+}
+);
+
+//Define a jewlery route
+$f3->route('GET /jewelry/rings/toe-rings', function() {
+    //echo'<h1>Buy a toe ring today</h1>';
+    $template = new Template();
+    echo $template->render('views/toe-rings.html');
 }
 );
 
---
+++ tmp/mpobqetxkze9.1ojftubl1yq3r.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Toe-Rings</title>
+</head>
+
+<body>
+
+<h1>Toe Rings</h1>
+
+</body>
+</html>
